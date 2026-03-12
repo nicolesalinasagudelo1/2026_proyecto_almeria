c10b17c9497833416f35b3753d6cc40e1f51c685
nicolesalinasagudelo1 <nicolesalinasagudelo1@gmail.com>
2026-03-12 18:09:14 +0100

    modificacion de prueba

--- Analisis_piezometros.R
+++ Analisis_piezometros.R
@@ -1,3 +1,11 @@
 
-###Subir R script sobre datos piezometricos. 
+###Subir R script sobre datos piezometricos.
+
+#Piezometros Nicole Salinas Agudelo: Campo de nijar, cuenca del río nacimiento
+#cubeta de ballabona - sierra lisbona, Bajo almanzora, Sierra de las estancias
+
+
+
+
+
 
